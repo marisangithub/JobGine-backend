f7046be389ca4553876a852c565ed65d6f2b0c04
marisangithub <123561031+marisangithub@users.noreply.github.com>
2026-06-12 16:33:51 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const dns = require("dns");
+
 const app = require("./app");
 
-dns.setDefaultResultOrder("ipv4first");
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
