e7a377c33fb9b4eabc40d66b2e550d8dfcb9dcaf
marisangithub <your-email@domain.com>
2026-06-12 17:03:08 +0530

    email-change

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+const dns = require("dns");
 
+dns.setDefaultResultOrder("ipv4first");
 const app = require("./app");
 
 
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
 
-const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendOTP = async (
   email,
